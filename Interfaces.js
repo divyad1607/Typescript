afc50616d165fbdd92b612c711cfd1a6425b42fe
Divya Deshmukh <divyad1607@gmail.com>
2025-11-21 12:02:15 +0530

    Add new file

---
+++ Interfaces.js
@@ -0,0 +1,12 @@
+var divya = { dbId: 22, email: "divya@google.com",
+    userId: 2211,
+    role: "admin",
+    githubToken: "github",
+    startTrail: function () {
+        return "trail started";
+    },
+    getCoupon: function (name, off) {
+        return 10;
+    }
+};
+divya.email = "divya@google.com";
--- Interfaces.ts
+++ Interfaces.ts
@@ -8,8 +8,17 @@ interface User {
     getCoupon(couponname: string, value: number): number
 }
 
-const divya: User = {dbId: 22, email:"divya@google.com",
+interface User {
+    githubToken: string}
+
+interface Admin extends User {
+    role: "admin" | "ta" | "leraner",
+}
+
+const divya: Admin = {dbId: 22, email:"divya@google.com",
 userId:2211,
+role: "admin",
+githubToken: "github",
 startTrail: () => {
     return "trail started"
 },
