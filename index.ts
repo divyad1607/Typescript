666df50d591cda6228e4637fedd06dc38638dae9
Divya Deshmukh <divyad1607@gmail.com>
2025-11-21 13:01:52 +0530

    Udpateing file

--- index.ts
+++ index.ts
@@ -9,6 +9,9 @@
    // }
 //}
 class User {
+
+    protected _courseCount = 1
+
     readonly city: string = "Jaipur"
     constructor(
         public email: string, 
@@ -17,6 +20,33 @@ class User {
     ){
 
     }
+    private deleteToken(){
+        console.log("Toen deleted")
+    }
+
+    get getAppleEmail(): string{
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum){
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount = courseNum
+    }
+}
+
+class SubUser extends User {
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
+    }
 }
 
 const Divya = new User("divyad@.com", "divya")
+
+//Divya.deleteToken()
