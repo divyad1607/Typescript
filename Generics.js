dc0503e59e2b5e4b9105b9aac747e6d0cb2f788d
Divya Deshmukh <divyad1607@gmail.com>
2025-11-22 12:43:56 +0530

    Add a new file

---
+++ Generics.js
@@ -0,0 +1,16 @@
+var score = [];
+var names = [];
+function identityOne(val) {
+    return val;
+}
+function identityTwo(val) {
+    return val;
+}
+function identityThree(val) {
+    return val;
+}
+//identityThree("Divya")
+function identityFour(val) {
+    return val;
+}
+//identityFour<Bottle>({})
--- index.ts
+++ index.ts
@@ -8,45 +8,45 @@
 
    // }
 //}
-class User {
+// class User {
 
-    protected _courseCount = 1
+//     protected _courseCount = 1
 
-    readonly city: string = "Jaipur"
-    constructor(
-        public email: string, 
-        public name: string,
-        //private userId: string,
-    ){
+//     readonly city: string = "Jaipur"
+//     constructor(
+//         public email: string, 
+//         public name: string,
+//         //private userId: string,
+//     ){
 
-    }
-    private deleteToken(){
-        console.log("Toen deleted")
-    }
+//     }
+//     private deleteToken(){
+//         console.log("Toen deleted")
+//     }
 
-    get getAppleEmail(): string{
-        return `apple${this.email}`
-    }
+//     get getAppleEmail(): string{
+//         return `apple${this.email}`
+//     }
 
-    get courseCount(): number {
-        return this._courseCount
-    }
+//     get courseCount(): number {
+//         return this._courseCount
+//     }
 
-    set courseCount(courseNum){
-        if (courseNum <= 1) {
-            throw new Error("Course count should be more than 1")
-        }
-        this._courseCount = courseNum
-    }
-}
+//     set courseCount(courseNum){
+//         if (courseNum <= 1) {
+//             throw new Error("Course count should be more than 1")
+//         }
+//         this._courseCount = courseNum
+//     }
+// }
 
-class SubUser extends User {
-    isFamily: boolean = true
-    changeCourseCount(){
-        this._courseCount = 4
-    }
-}
+// class SubUser extends User {
+//     isFamily: boolean = true
+//     changeCourseCount(){
+//         this._courseCount = 4
+//     }
+// }
 
-const Divya = new User("divyad@.com", "divya")
+// const Divya = new User("divyad@.com", "divya")
 
 //Divya.deleteToken()
